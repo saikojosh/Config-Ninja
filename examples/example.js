8a940c708f3eaab6eced8f2ed8c9dcb2c7e90b8c
Josh Cole <saikojosh@gmail.com>
2016-03-31 11:19:13 +0100

    Updated the example

---
+++ examples/example.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+ * EXAMPLE
+ * An example for how to use Config-Ninja.
+ */
+
+// Prepare the ninja on application load.
+const configInModuleA = require('../configNinja').init(__dirname + '/cfg', null, {
+  additionalMergeFiles: ['local'],
+});
+
+// In another module we need config again...
+const configinModuleB = require('../configNinja');
+
+// Use our config.
+console.dir(configinModuleB);
+console.log('Nested Number:', configinModuleB.nested.number);
