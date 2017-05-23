8c4fbbe9c228d4a4200d0d2c1c48f04722b35f38
Josh Cole <saikojosh@users.noreply.github.com>
2017-05-23 15:57:41 +0100

    Rewrite config ninja to prevent issues with module caching inside dependencies

--- configNinja.js
+++ configNinja.js
@@ -4,168 +4,249 @@
  * CONFIG-NINJA
  */
 
-const ME = module.exports;
-const fs = require('fs');
-const path = require('path');
-const clc = require('cli-color');
-const extender = require('object-extender');
+const stackTrace = require(`stack-trace`);
+const extender = require(`object-extender`);
+const ConfigBuilder = require(`./modules/configBuilder`);
+const ConfigError = require(`./modules/configError`);
+
+const configCache = {};
 
 /*
- * Loads in a config file and converts it to a JSON string ready for parsing, whilst handling errors.
+ * Utility function to the return the options used to construct the config and its meta information.
  */
-const readConfigFile = function (type, name, dir, absolutePath, configInFilename, ignoreMissing) {
+function __inspect (object) {
 
-  let filename = path.join(dir, `${name}${configInFilename ? '.config' : ''}.json`);
-  let cfg;
-
-  // If the user is not passing in an absolute path to the config then we use the current working directory.
-  if (!absolutePath) { filename = path.join(process.cwd(), filename); }
-
-  try {
-    cfg = fs.readFileSync(filename).toString();
-  } catch (err) {
-    if (!ignoreMissing) {
-      throw new Error(clc.redBright(`Unable to read ${type} config "${name}" (${err.code}) from path "${filename}".`));
-    }
-  }
-
-  return cfg || null;
-
-};
-
-/*
- * Takes a JSON string and returns a nicely formatted config object, whilst handling errors.
- */
-const parseConfigJSON = function (type, name, input) {
-
-  let output;
-
-  try {
-    output = JSON.parse(input);
-  } catch (err) {
-    throw new Error(clc.redBright(`The ${type} config "${name}" is not valid JSON (${err.name}).`));
-  }
-
-  return output;
+	return {
+		options: extender.clone(object.options),
+		meta: extender.clone(object.meta),
+	};
 
 }
 
 /*
- * Load and merge the config files synchronously.
+ * Reload the existing config and return the result. If the immutable option is not set, the original config object will
+ * be mutated.
  */
-ME.init = function (dir, env, _options) {
-
-  // Default options.
-  let options = extender.defaults({
-    configInFilename: true,
-    setEnvProperty: { 'production': 1, 'staging': 2, 'development': 3 },
-    additionalMergeFiles: [],
-    ignoreMissingAdditional: true,
-    absolutePath: false,
-    returnCopy: false,
-  }, _options);
-
-  // If no dir is specified assume we are reloading.
-  if (!dir) {
-    dir = ME._cfgPath;
-    if (!env) { env = ME._env; }
-    options = extender.defaults(ME._options, _options);
-  }
-
-  // Which environment are we operating in?
-  if (!env) { env = process.env.NODE_ENV || 'development'; }
-
-  const configList = [];
-  let prodCfg;
-  let envCfg;
-  let merged;
-
-  // Prepare the production config.
-  prodCfg = readConfigFile('production', 'production', dir, options.absolutePath, options.configInFilename);
-  prodCfg = parseConfigJSON('production', 'production', prodCfg);
-
-  // Load the environment config?
-  if (env !== 'production') {
-    envCfg = readConfigFile('environment', env, dir, options.absolutePath, options.configInFilename);
-    envCfg = parseConfigJSON('environment', env, envCfg);
-  }
-
-  // Add in our internal properties.
-  const defaults = {
-    _env: env,
-    _cfgPath: dir,
-    _options: options,
-  };
-
-  // Prepare the list of config objects.
-  configList.push(defaults, prodCfg, envCfg);
-
-  // Do we have any additional files to merge in?
-  if (options.additionalMergeFiles && options.additionalMergeFiles.length) {
-
-    for (var a = 0, alen = options.additionalMergeFiles.length; a < alen; a++) {
-      const additionalMergeFile = options.additionalMergeFiles[a];
-      let addCfg;
-
-      // Attempt to load in the config file.
-      addCfg = readConfigFile('additional', additionalMergeFile, dir, options.absolutePath, options.configInFilename, options.ignoreMissingAdditional);
-      if (!addCfg) { continue; }
-
-      // JSONify.
-      addCfg = parseConfigJSON('additional', additionalMergeFile, addCfg);
-
-      // Store the additional config ready for merging.
-      configList.push(addCfg);
-    }
-
-  }
-
-  // Merge the configs together.
-  merged = extender.merge(...configList);
-
-  // Add the env properties?
-  if (options.setEnvProperty && !merged.env) {
-    const environmentLevels = (typeof options.setEnvProperty === 'object' ? options.setEnvProperty : null);
-    merged.env = {
-      id: env,
-      level: (environmentLevels && typeof environmentLevels[env] !== 'undefined' ? environmentLevels[env] : null),
-    };
-  }
-
-  // Drop out here if we just need to return a copy of the config.
-  if (options.returnCopy) { return merged; }
-
-  // Copy the configs onto the 'config-ninja' object.
-  extender.mergeInto(ME, merged);
-
-  // Allow immediate use of 'config'.
-  return ME;
-
-};
+function __reload (object) {
+	return prepareConfig(object.configId, null, null, object.options);
+}
 
 /*
- * Returns a temporary copy of the given config.
+ * Switch the config to a new environment and return the result. If the immutable option is not set, the original config
+ * object will be mutated.
  */
-ME.get = function (useEnv, raw) {
+function __switch (object, env) {
 
-  // Must initialise the config first.
-  if (!ME._env) { throw new Error('Config has not been initialised yet.'); }
+	if (!env) { throw new ConfigError(`You must specify an environment to switch to.`); }
 
-  let useOptions = extender.defaults(ME._options, {
-    returnCopy: true,
-  });
+	const newOptions = extender.merge(object.options, { env });
+	return prepareConfig(object.configId, null, null, newOptions);
 
-  // Return just a single file without merging it.
-  if (raw) {
-    let rawCfg;
+}
 
-    // Prepare the config.
-    rawCfg = readConfigFile('raw', env, useOptions, dir, useOptions.absolutePath, useOptions.configInFilename);
-    rawCfg = parseConfigJSON('raw', env, rawCfg);
-    return rawCfg;
-  }
+/*
+ * Reload the existing config and return the result, adding in the extra local config files specified. If the immutable
+ * option is not set, the original config object will be mutated.
+ */
+function __addLocal (object, _localConfig) {
 
-  // Return a copy of the given config.
-  return ME.init(ME._cfgPath, useEnv, useOptions);
+	const localConfig = (typeof _localConfig === `string` ? [_localConfig] : _localConfig);
 
+	if (!Array.isArray(localConfig)) { throw new ConfigError(`You must specify an array of local config files to add.`); }
+
+	const newOptions = extender.merge(object.options, { localConfig: localConfig.concat(localConfig) });
+	return prepareConfig(object.configId, null, null, newOptions);
+
+}
+
+/*
+ * Gets the specified environment's config as if that were the new environment and return the result. Does not modify
+ * the existing config.
+ */
+function __get (object, env) {
+
+	if (!env) { throw new ConfigError(`You must specify an environment to get.`); }
+
+	const newOptions = extender.merge(object.options, { env });
+	return prepareConfig(object.configId, null, null, newOptions, true);
+
+}
+
+/*
+ * Utiliy function to return the raw config values for the given environment before they were merged in.
+ */
+function __raw (object, env) {
+
+	// If no environment is specified return all the raw files in a hash.
+	if (!env) { return extender.clone(object.raw); }
+
+	if (!object.raw[env]) { throw new ConfigError(`There is no config loaded called "${env}".`); }
+
+	return extender.clone(object.raw[env]);
+}
+
+/*
+ * Utility function to return the trace of the stack that initialised the config.
+ */
+function __trace (object) {
+	return Array.from(object.trace);
+}
+
+/*
+ * Handle the different function signatures for .init().
+ */
+function initParameterSwitching (param2, param3, param4) {
+
+	let dir = null;
+	let env = null;
+	let options = null;
+
+	// .init(configId, dir, env, options);
+	if (typeof param2 !== `undefined` && typeof param3 !== `undefined` && typeof param4 !== `undefined`) {
+		dir = param2;
+		env = param3;
+		options = param4;
+	}
+
+	// .init(configId, dir, env);
+	else if (typeof param2 !== `undefined` && typeof param3 === `string` && typeof param4 === `undefined`) {
+		dir = param2;
+		env = param3;
+	}
+
+	// .init(configId, dir, options);
+	else if (typeof param2 !== `undefined` && typeof param3 === `object` && typeof param4 === `undefined`) {
+		dir = param2;
+		options = param3;
+	}
+
+	// .init(configId, dir);
+	else if (typeof param2 === `string` && typeof param3 === `undefined` && typeof param4 === `undefined`) {
+		dir = param2;
+	}
+
+	// .init(configId, options);
+	// .init(configId);
+	else {
+		options = param2;
+	}
+
+	return { dir, env, options: options || {} };
+
+}
+
+/*
+ * Factory to create a new config. Can only be used once per config id.
+ */
+function init (configId, param2, param3, param4) {
+
+	// We can only initialise each config id once.
+	if (configCache[configId]) {
+		throw new ConfigError(`The config "${configId}" already exists! Use ".use()" to obtain an immutable copy of it.`);
+	}
+
+	const { dir, env, options } = initParameterSwitching(param2, param3, param4);
+
+	return prepareConfig(configId, dir, env, options);
+
+}
+
+/*
+ * Creates a new config.
+ */
+function prepareConfig (configId, dir, env, options, returnOnly = false) {
+
+	const builder = new ConfigBuilder(configId, dir, env, options);
+
+	// Just returns the config values without storing the config.
+	if (returnOnly) { return extender.clone(builder.getValues()); }
+
+	// The config has previously been loaded and we the immutable option is not set.
+	if (configCache[configId] && !options.immutable) {
+
+		// Remove all old properties from the config values so it's empty without destroying the values object itself.
+		for (const key in configCache[configId].values) {
+			if (configCache[configId].values.hasOwnProperty(key)) {
+				delete configCache[configId].values[key];
+			}
+		}
+
+		// Merge in the new config.
+		extender.mergeInto(configCache[configId].values, builder.getValues());
+
+		// Overwrite these with the new values (rather than merging).
+		configCache[configId].options = builder.getOptions();
+		configCache[configId].meta = builder.getMeta();
+		configCache[configId].raw = builder.getRaw();
+
+	}
+
+	// Otherwise the immutable option is set, or this is the first init of the config.
+	else {
+
+		configCache[configId] = {
+			configId,
+			values: builder.getValues(),
+			options: builder.getOptions(),
+			meta: builder.getMeta(),
+			raw: builder.getRaw(),
+			trace: stackTrace.get().map(item => item.toString()),
+		};
+
+	}
+
+	// Return the config.
+	const immutable = options && options.immutable;
+	return use(configId, immutable);
+
+}
+
+/*
+ * Returns a copy of the config in memory.
+ */
+function use (configId, immutable = false) {
+
+	// Ensure the config was previously initialised.
+	if (!configCache[configId]) {
+		throw new ConfigError(`The config "${configId}" has not been initialised with the ".init()" method!`);
+	}
+
+	// Create a copy to make the config immutable if the immutable option is truthy.
+	const object = configCache[configId];
+	const config = (immutable ? extender.clone(object.values) : object.values);
+
+	// Add some utility functions to the config.
+	config.__inspect = __inspect.bind(null, object);
+	config.__reload = __reload.bind(null, object);
+	config.__switch = __switch.bind(null, object);
+	config.__addLocal = __addLocal.bind(null, object);
+	config.__get = __get.bind(null, object);
+	config.__raw = __raw.bind(null, object);
+	config.__trace = __trace.bind(null, object);
+
+	return config;
+
+}
+
+/*
+ * Removes an existing config, without preventing any other modules from using it if they have already obtained a copy.
+ */
+function wipe (configId) {
+
+	// Ensure the config was previously initialised.
+	if (!configCache[configId]) {
+		throw new ConfigError(`The config "${configId}" has not been initialised with the ".init()" method!`);
+	}
+
+	delete configCache[configId];
+
+}
+
+/*
+ * Export.
+ */
+module.exports = {
+	init,
+	use,
+	wipe,
 };
---
+++ modules/configBuilder.js
@@ -0,0 +1,182 @@
+'use strict';
+
+const fileSystem = require(`fs`);
+const path = require(`path`);
+const extender = require(`object-extender`);
+const ConfigError = require(`./configError`);
+
+module.exports = class ConfigBuilder {
+
+	/*
+	 * Instantiate a new config instance.
+	 */
+	constructor (configId, dir = null, env = null, _options = null) {
+		this.init(configId, dir, env, _options);
+	}
+
+	/*
+	 * Returns an absolute path to the config directory. If the user is not passing in an absolute path to the config then
+	 * we use the current working directory as the base for the config directory path.
+	 */
+	prepareAbsoluteDirPath (_dir, defaultDir) {
+		const dir = _dir || defaultDir;
+		return (path.isAbsolute(dir) ? dir : path.join(process.cwd(), dir));
+	}
+
+	/*
+	 * Initialise the config.
+	 */
+	init (configId, dir = null, env = null, options = null) {
+
+		if (!configId) { throw new ConfigError(`You must provide a valid config id!`); }
+
+		const defaultOptions = {
+			dir: this.prepareAbsoluteDirPath(dir, `./config`),
+			env: env || process.env.NODE_ENV || `development`,
+			shortFilenames: false,
+			environmentLevels: { production: 1, staging: 2, development: 3 },
+			localConfig: [`local`],
+			requireLocalConfig: false,
+			immutable: false,
+		};
+
+		// Set the builder's configuration.
+		this.configId = configId;
+		this.options = extender.defaults(defaultOptions, options);
+		this.meta = {
+			loadedOn: 0,
+			loadedFiles: [],
+		};
+		this.rawFiles = {};
+		this.configValues = {};
+
+		// Ensure the localConfig option doesn't have any duplicates.
+		if (Array.isArray(this.options.localConfig)) {
+			this.options.localConfig = Array.from(new Set(this.options.localConfig));
+		}
+
+		// (Re)-load all the config files.
+		this.loadFiles();
+
+	}
+
+	/*
+	 * Read and parse all the config files.
+	 */
+	loadFiles () {
+
+		const mergeList = [];
+		const files = [{ type: `production`, filename: `production` }];
+
+		// Add the environent config if it isn't the production config.
+		if (this.options.env !== `production`) {
+			files.push({ type: this.options.env, filename: this.options.env });
+		}
+
+		// Add the additional local files to the files list, if any.
+		if (Array.isArray(this.options.localConfig)) {
+			this.options.localConfig.forEach(filename => files.push({ type: `local`, filename }));
+		}
+
+		// Parse each config file in order.
+		files.forEach(file => {
+
+			// Load the next file.
+			const json = this.readConfigFile(file.type, file.filename);
+			if (!json) { return; }
+
+			// Parse and save the values.
+			const values = this.parseConfigJson(file.type, file.filename, json);
+			this.rawFiles[file.filename] = values;
+			mergeList.push(values);
+
+		});
+
+		// Generate the config by merging it all together in order.
+		this.configValues = extender.merge(...mergeList);
+
+		// Automatically add the environment information to the config if 'env' isn't used for something else.
+		if (!this.configValues.env && this.options.environmentLevels) {
+			/* eslint id-length: 0 */
+			this.configValues.env = {
+				id: this.options.env,
+				level: this.options.environmentLevels[this.options.env] || `-not set-`,
+			};
+		}
+
+		// Update the meta data.
+		this.meta.loadedOn = Date.now();
+		this.meta.loadedFiles = files;
+
+	}
+
+	/*
+	 * Loads in a config file and converts it to a JSON string ready for parsing, whilst handling errors.
+	 */
+	readConfigFile (type, filename) {
+
+		const fullFilename = path.join(this.options.dir, `${filename}${this.options.shortFilenames ? `` : `.config`}.json`);
+		let json;
+
+		try {
+			json = fileSystem.readFileSync(fullFilename).toString();
+		}
+		catch (err) {
+			if (type !== `local` || (type === `local` && this.options.requireLocalConfig)) {
+				const errInfo = err.id || err.code || err.name;
+				throw new ConfigError(`Unable to read ${type} config "${filename}" from path "${fullFilename}" (${errInfo}).`);
+			}
+		}
+
+		return json || null;
+
+	}
+
+	/*
+	 * Takes a JSON string and returns a nicely formatted config object, whilst handling errors.
+	 */
+	parseConfigJson (type, filename, json) {
+
+		let output;
+
+		try {
+			output = JSON.parse(json);
+		}
+		catch (err) {
+			const errInfo = err.id || err.code || err.name;
+			throw new ConfigError(`The ${type} config "${filename}" is not valid JSON (${errInfo}).`);
+		}
+
+		return output;
+
+	}
+
+	/*
+	 * Returns a copy of the merged config.
+	 */
+	getValues () {
+		return extender.clone(this.configValues);
+	}
+
+	/*
+	 * Returns a copy of the options used to construct this instance of the config.
+	 */
+	getOptions () {
+		return extender.clone(this.options);
+	}
+
+	/*
+	 * Returns a copy of the meta information for this instance of the config.
+	 */
+	getMeta () {
+		return extender.clone(this.meta);
+	}
+
+	/*
+	 * Returns a copy of all the raw config files as they were when loaded.
+	 */
+	getRaw () {
+		return extender.clone(this.rawFiles);
+	}
+
+};
