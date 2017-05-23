1cf006b128c3df2fe76b852243cc72473f5593f4
Josh Cole <saikojosh@users.noreply.github.com>
2017-05-23 16:28:46 +0100

    Update example

--- examples/example.js
+++ examples/example.js
@@ -5,15 +5,17 @@
  * An example for how to use Config-Ninja.
  */
 
+/* eslint no-console: 0 */
+
+// Ensure we always work relative to this script.
+process.chdir(__dirname);
+
 // Prepare the ninja on application load.
-const configInModuleA = require('../configNinja').init(__dirname + '/cfg', null, {
-  additionalMergeFiles: ['local'],
-  absolutePath: true,
-});
+const configInModuleA = require(`../configNinja`).init(`my-config`);
 
 // In another module we need config again...
-const configinModuleB = require('../configNinja');
+const configInModuleB = require(`../configNinja`).use(`my-config`);
 
-// Use our config.
-console.dir(configinModuleB);
-console.log('Nested Number:', configinModuleB.nested.number);
+console.log(`Config:`, configInModuleA);
+console.log(`\nNested Number: ${configInModuleB.nested.number}\n`);
+console.log(`Plain:`, configInModuleA.__plain());
